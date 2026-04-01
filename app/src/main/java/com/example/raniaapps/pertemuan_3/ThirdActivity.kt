d8f3f502b06b320f64b089aa876e41e417a777a5
wanrania <wan24si@mahasiswa.pcr.ac.id>
2026-04-01 16:44:12 +0700

    pertemuan 3 commit

---
+++ app/src/main/java/com/example/raniaapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,47 @@
+package com.example.raniaapps.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.raniaapps.R
+import com.example.raniaapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+//        setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inisialisasi komponen
+        // val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+        // val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val inputNoTujuan = binding.inputNoTujuan.text
+
+            Toast.makeText(this, "Pesan berhasil dikirim ke = $inputNoTujuan", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
