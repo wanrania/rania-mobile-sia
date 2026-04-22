46c298591c5b0ed0906a0614741531eaff05ec88
wanrania <wan24si@mahasiswa.pcr.ac.id>
2026-04-22 16:21:25 +0700

    Pertemuan 6 : SplashScreen

--- app/src/main/java/com/example/raniaapps/AuthActivity.kt
+++ app/src/main/java/com/example/raniaapps/AuthActivity.kt
@@ -3,7 +3,6 @@ package com.example.raniaapps
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.raniaapps.databinding.ActivityAuthBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -25,12 +24,12 @@ class AuthActivity : AppCompatActivity() {
         val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
 
         //Kondisi jika isLogin bernilai true
-        val isLogin = sharedPref.getBoolean("isLogin", false)
-        if (isLogin) {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+//        val isLogin = sharedPref.getBoolean("isLogin", false)
+//        if (isLogin) {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
         binding.btnLogin.setOnClickListener {
 
             val username = binding.etUsername.text.toString()
